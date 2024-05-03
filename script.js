41b0c5f46494cf9967bd0c121f1020de1929fc10
DedB0r0ded <revikad.00@mail.ru>
2024-05-03 21:07:06 +0300

    Created script file. Created a book proto

---
+++ script.js
@@ -0,0 +1,15 @@
+let book = {
+    m_price: 0,
+    m_pages: 3,
+    m_currentPage: 1,
+
+    create: (price = 0, pages = 3, currentPage = 1) =>{
+        let book = Object.create(this)
+        book.m_price = price
+        book.m_pages = pages
+        book.m_currentPage = currentPage
+        return book
+    }
+
+
+}
